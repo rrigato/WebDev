1aa09f5bdbfdf377d4be6384913da166576e7265
rrigato <rrigato@horizon.csueastbay.edu>
2016-01-25 20:18:20 -0800

    Creating a login for the project

---
+++ CS3520Winter20162/src/java/LoginCreate/LoginCreate.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LoginCreate;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Punkiehome1
+ */
+public class LoginCreate extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+
+                String url = "/createAccount.jsp";
+                String email = request.getParameter("email");
+                String username = request.getParameter("username");
+                String msg;
+                String formType = request.getParameter("formType");
+                if (formType.equals("create")){
+                    request.setAttribute("msg", "Nice try tying to create an account, I haven't set up the database");
+                    request.setAttribute("email",email);
+                    request.setAttribute("username", username);
+      
+
+                }else if (formType.equals("login")){
+                    request.setAttribute("msg", "Nice try tying to login, I haven't set up the database");
+                  
+                }
+        this.getServletContext().getRequestDispatcher(url).forward(request, response);
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ CS3520Winter20162/web/createAccount.jsp
@@ -0,0 +1,18 @@
+<%-- 
+    Document   : create
+    Created on : Jan 24, 2016, 3:22:38 PM
+    Author     : Punkiehome1
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Ryan Rigato HW #2</title>
+    </head>
+    <body>
+        <h1>${msg}</h1>
+        <p></p>
+    </body>
+</html>
